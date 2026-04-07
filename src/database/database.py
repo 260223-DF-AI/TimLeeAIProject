42275bd9904f06a1904d953d49a6a000bede5ce0
leew735 <amelia735@revature.net>
2026-04-07 09:13:20 -0500

    edit .gitignore to cut out irrelevant parts

--- src/database/database.py
+++ src/database/database.py
@@ -1,1 +1,17 @@
+# main imports
+import psycopg2
+
+# project imports
 from src.utils import logger
+
+def create_db():
+    logger.debug("Attempting db creation...")
+    
+
+def connect_db():
+    logger.debug("Attempting db connection...")
+
+if __name__ == "__main__":
+    create_db()
+    connect_db()
+
