8b8bd760ab4b126373e62bf1e38210bdb3da4f3a
leew735 <amelia735@revature.net>
2026-04-10 10:53:12 -0500

    can fully partition/process dataset

--- src/database/data_processor.py
+++ src/database/data_processor.py
@@ -12,17 +12,26 @@ The script here will do the unzipping/processing for you so just drop the zip fi
 # main imports
 import zipfile 
 import os
+import shutil
 
 # project imports
 from src.utils import logger
 from src.paths import DATA_ROOT
+
+# path constants
+IMGS = DATA_ROOT / "imgs"
+TRAIN = DATA_ROOT / "imgs/train"
+VAL = DATA_ROOT / "imgs/val"
+TEST = DATA_ROOT / "imgs/test"
+UNLABELED = DATA_ROOT / "imgs/unlabeled"
+FUTURE_TRAIN = DATA_ROOT / "imgs/future_train"
 
 logger = logger.setup_logger(__name__, "debug")
 
 def unzip_dataset() -> None:
     """Checks that data exists & unzips it."""
 
-    logger.debug("Unzipping files, this will take about a minute.")
+    logger.debug("Unzipping files, this will take about two minutes.")
     try:
         with zipfile.ZipFile(DATA_ROOT / "state-farm-distracted-driver-detection.zip", "r") as zip_ref:
             zip_ref.extractall(DATA_ROOT)
@@ -36,20 +45,34 @@ def unzip_dataset() -> None:
 
     logger.debug("Finished dataset processing.")
 
+def clear_folders_to_reprocess():
+    """Mostly for testing. Clears out all the folders for reprocessing."""
+
+    logger.debug("Clearing out folders for reprocessing, this will take about 30 seconds.")
+
+    try:
+        shutil.rmtree(IMGS)
+        os.remove(DATA_ROOT / "driver_imgs_list.csv")
+        os.remove(DATA_ROOT / "sample_submission.csv")
+    except Exception as e:
+        logger.error(f"Error type {type(e)}: {e}")
+    else:
+        logger.info("Successfully cleared out folders for reprocessing.")
+    
+
 def process_dataset():
     """Processes the dataset with these steps:
     1. Rename existing data/imgs/test folder to data/imgs/unlabeled
     2. Partition existing train data into train/val/test.
-    
     """
 
     logger.debug("Processing dataset...")
 
     # rename old "test" folder
     try:
-        if os.path.exists(DATA_ROOT / "imgs/unlabeled"):
+        if os.path.exists(UNLABELED):
             raise FileExistsError
-        os.rename(DATA_ROOT / "imgs/test", DATA_ROOT / "imgs/unlabeled")
+        os.rename(TEST, UNLABELED)
     except FileExistsError:
         logger.info("Unlabeled folder already exists. Skipping.")
     except FileNotFoundError as e:
@@ -61,19 +84,20 @@ def process_dataset():
 
     # verify train exists, create new val & test folders/subfolders
     try:
-        train = DATA_ROOT / "imgs/train"
-        if not os.path.exists(train):
+        if not os.path.exists(TRAIN):
             raise FileNotFoundError
-        
-        val = DATA_ROOT / "imgs/val"
-        test = DATA_ROOT / "imgs/test"
-        os.makedirs(val)
-        os.makedirs(test)
+
+        os.makedirs(VAL)
+        os.makedirs(TEST)
+        os.makedirs(FUTURE_TRAIN)
 
         for i in range (0, 7):
-            os.makedirs(f"{val}/c{i}")
-            os.makedirs(f"{test}/c{i}")
+            os.makedirs(f"{VAL}/c{i}")
+            os.makedirs(f"{TEST}/c{i}")
+            os.makedirs(f"{FUTURE_TRAIN}/c{i}")
 
+    except FileExistsError:
+        logger.info("Val/test folders already exist. Skipping.")
     except FileNotFoundError:
         logger.error("Couldn't find train folder. Did you unzip the dataset first?")
     except Exception as e:
@@ -81,20 +105,62 @@ def process_dataset():
     else:
         logger.info("Successfully partitioned train data.")
 
-    logger.debug("Finished processing dataset.")
+    # create map for consolidating c1-c4 in the original database to one category, c1
+    category_map = {
+        "c0": "c0",
+        "c1": "c1", 
+        "c2": "c1",
+        "c3": "c1",
+        "c4": "c1",
+        "c5": "c2",
+        "c6": "c3",
+        "c7": "c4",
+        "c8": "c5",
+        "c9": "c6"
+    }
 
     # take 1 of every 9 files for test/val
     try:
+        counter = 0
         # for each subfolder in train
-        pass
+        for category in os.listdir(TRAIN):
+            for image in os.listdir(f"{TRAIN}/{category}"):
+                # map new categories, consolidating the old c1-c4
+                new_category = category_map[category]
+
+                # move 1/9 to val
+                if counter % 9 == 0:
+                    os.rename(f"{TRAIN}/{category}/{image}", f"{VAL}/{new_category}/{image}")
+                # move 1/9 to test
+                elif counter % 9 == 1:
+                    os.rename(f"{TRAIN}/{category}/{image}", f"{TEST}/{new_category}/{image}")
+                # correctly categorize the rest & separate them to a new dir to prevent
+                # accidentally going over them again
+                else:
+                    os.rename(f"{TRAIN}/{category}/{image}", f"{FUTURE_TRAIN}/{new_category}/{image}")
+                counter += 1
+            
     except Exception as e:
-        logger.error(f"Error type {type(e)}: {e}")
+        logger.error(f"Error type {type(e)}: {e}, line {e.__traceback__.tb_lineno}")
     else: 
         logger.info("Successfully partitioned train data.")
+
+    # clean up/rename
+    try:
+        # now that exising train should contain only empty folders, remove them & train
+        shutil.rmtree(TRAIN)
+
+        # rename future_train to train
+        os.rename(FUTURE_TRAIN, TRAIN)
+    except Exception as e:
+        logger.error(f"Error type {type(e)}: {e}")
+    else:
+        logger.debug("Successfully cleaned up train data.")
 
     logger.debug("Finished processing dataset.")
 
 # for testing purposes
 if __name__ == "__main__":
-    #unzip_dataset()
+    clear_folders_to_reprocess()
+    unzip_dataset()
     process_dataset()
